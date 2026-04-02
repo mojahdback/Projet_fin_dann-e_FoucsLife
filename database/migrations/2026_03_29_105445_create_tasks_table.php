83c54bfdadb113e3abd69a231f3d0e691bed69ce
mohamed <mojahdontheway@gmail.com>
2026-04-02 02:02:45 +0200

    feat: implement tasks tables

--- database/migrations/2026_03_29_105445_create_tasks_table.php
+++ database/migrations/2026_03_29_105445_create_tasks_table.php
@@ -12,7 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->id('task_id');
+            $table->foreignId('user_id')
+                  ->constrained('users', 'user_id')
+                  ->onDelete('cascade');
+            $table->foreignId('goal_id')
+                  ->nullable()
+                  ->constrained('goals', 'goal_id')
+                  ->onDelete('set null');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('priority', ['low' , 'medium' , 'high'])->default('medium');
+            $table->enum('status', ['todo' , 'in_progress' , 'done' ,'cancelled'])->default('todo');
+            $table->enum('period' , ['day' , 'week' , 'month' ,'year'])->default('day');
+            $table->date('due_date')->nullable();
             $table->timestamps();
         });
     }
