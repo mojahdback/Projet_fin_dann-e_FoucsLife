4997d946a160df1b95b6d118dd876965611e7c80
mohamed <mojahdontheway@gmail.com>
2026-04-07 16:00:00 +0200

    feat(auth): add AuthRepository for user authentication queries

---
+++ app/Repositories/AuthRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repositories;
+
+use APP\Models\User;
+
+class AuthRepository
+{
+    public function findByEmail(string $email): ?User
+    {
+        return User::where('email' , $email)->first();
+    }
+
+
+    
+    public function create(array $date): User
+    {
+       return User::create([
+        'name' => $date['name'],
+        'email'  => $date['email'],
+        'password' => bcrypt($date['password']),
+        'role'  => 'user' ,
+        ]);
+    }
+
+}
